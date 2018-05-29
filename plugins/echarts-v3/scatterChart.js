fb10eae6dc59cb22ab7e21c86f8ce2159d7ef5c9
coffeecoke <18601376034@163.com>
2018-05-29 11:02:46 +0800

    t0529b

--- plugins/echarts-v3/scatterChart.js
+++ plugins/echarts-v3/scatterChart.js
@@ -2,6 +2,7 @@
 
 $(function () {
     var eChart = echarts.init(document.getElementById('scatterChart1'));
+    var data =[10, 30, 50, 80, 90, 100,120];
     var option = {
         tooltip: {
             trigger: 'axis',
@@ -9,15 +10,21 @@ $(function () {
                 type: 'none'
             },
             backgroundColor: '#fff',  // 提示背景颜色，默认为透明度为0.7的黑色
-            borderColor: '#ccc',            // 提示边框颜色
+            borderColor: '#ccc',      // 提示边框颜色
             borderRadius: 4,
             padding:10,
             textStyle:{
                 color:"#000"
             }, 
             formatter: function (params) {
-                return "桥梁编号" + ': ' + params[0].name+"<br/>"+
-                        "维修量"+":"+params[0].data
+                
+                var totle = 0;
+                for(var i=0;i<=params[0].dataIndex;i++) {
+                    console.log(data[i])
+                    totle += data[i]
+                };
+                return "未来" + ': ' + params[0].name+"<br/>"+
+                        "维修量"+":"+totle
             },
             extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3)'    
         },
@@ -44,17 +51,18 @@ $(function () {
         },
         color: ['#3494fd'],
         grid: {
-            left: '0',
-            right: '0',
+            left: '2%',
+            right: '3%',
             bottom: '0',
             containLabel: true
         },
         series: [{
             name: 'hill',
             type: 'pictorialBar',
-            barCategoryGap: '-40%',
+            barCategoryGap: '-100%',
             // symbol: 'path://M0,10 L10,10 L5,0 L0,10 z',
             symbol: 'path://M0,10 L10,10 C5.5,10 5.5,5 5,0 C4.5,5 4.5,10 0,10 z',
+            symbolSize:['50%','100%'],
             itemStyle: {
                 normal: {
                     opacity: 0.5
@@ -63,34 +71,8 @@ $(function () {
                     opacity: 1
                 }
             },
-            data: [10, 30, 50, 80, 90, 100,120],
+            data:data,
             z: 10
-        }, {
-            name: 'glyph',
-            type: 'pictorialBar',
-            barGap: '-120%',
-            symbolPosition: 'end',
-            symbolSize: 0,
-            symbolOffset: [0, '-140%'],
-            // data: [{
-            //     value: 120,
-
-            // }, {
-            //     value: 60,
-
-            // }, {
-            //     value: 70,
-
-            // }, {
-            //     value: 90,
-
-            // }, {
-            //     value: 60,
-
-            // }, {
-            //     value: 120,
-
-            // }]
         }]
     };
     eChart.setOption(option);
