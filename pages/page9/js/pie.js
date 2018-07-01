ba78590ddd619e2a1c8d708e46b93a88fbf006b0
zhaoqian <zhaoqian@zhaoqian.local>
2018-07-01 18:18:34 +0800

    page

--- pages/page9/js/pie.js
+++ pages/page9/js/pie.js
@@ -1,11 +1,12 @@
 $(function () {
     var myChart6 = echarts.init(document.getElementById('pieChart6'));
 /*---------------------数据----------------------------*/
+/*---------------------数据----------------------------*/
 var exemptData = [{
-    value: 18,
-    name: '免考人数'
+    value: 60,
+    name: '已列入维修计划'
 }, {
-    value: 70,
+    value: 20,
     name: '',
     labelLine: {
         normal: {
@@ -15,9 +16,9 @@ var exemptData = [{
 }];
 var examNum = [{
     value: 48,
-    name: '正常考试'
+    name: '未列入维修计划',
 }, {
-    value: 28,
+    value: 58,
     name: '',
     labelLine: {
         normal: {
@@ -35,10 +36,10 @@ var exemptcolor = [{
     y2: 1,
     colorStops: [{
         offset: 0.2,
-        color: '#ffce22' // 0% 处的颜色
+        color: '#328cfe' // 0% 处的颜色
     }, {
         offset: 1,
-        color: '#ffce22' // 100% 处的颜色
+        color: '#328cfe' // 100% 处的颜色
     }],
     globalCoord: false // 缺省为 false
 }, 'none'];
@@ -51,34 +52,34 @@ var examNumcolor = [{
     y2: 1,
     colorStops: [{
         offset: 0.3,
-        color: '#328cfe' // 0% 处的颜色
+        color: '#ffce22' // 0% 处的颜色
     }, {
         offset: 1,
-        color: '#328cfe' // 100% 处的颜色
+        color: '#ffce22' // 100% 处的颜色
     }],
     globalCoord: false // 缺省为 false
 }, 'none'];
 /*---------------------富文本----------------------------*/
 var rich = {
     exemptname: {
-        color: '#115b70',
-        fontSize: 14 * scale,
+        color: '#000',
+        fontSize: 12 * scale,
         padding: [0, 0]
     },
     examptdata: {
-        color: '#115b70',
-        fontSize: 30 * scale,
+        color: '#000',
+        fontSize: 12 * scale,
         padding: [0, 0],
         fontWeight: 'bold'
     },
     exemname: {
-        color: '#115b70',
-        fontSize: 14 * scale,
+        color: '#000',
+        fontSize: 12 * scale,
         padding: [0, 0]
     },
     examdata: {
-        color: '#3fdaff',
-        fontSize: 30 * scale,
+        color: '#000',
+        fontSize: 12 * scale,
         padding: [0, 0],
         fontWeight: 'bold'
     },
@@ -86,13 +87,13 @@ var rich = {
         width: 14,
         height: 14,
         borderRadius:3,
-        backgroundColor: "#3fdaff",
+        backgroundColor: "#ffce22",
     },
     rectred: {
         width:14,
         height:14,
         borderRadius:3,
-        backgroundColor:'#eb297d'
+        backgroundColor:'#ffce22'
     },
     space: {
         padding: [0, 10, 0, 0]
@@ -101,19 +102,7 @@ var rich = {
 
 
 var option6 = {
-    backgroundColor: '#031f2d',
-    title: [{
-        text: '整体考试情况',
-        x: 'center',
-        y: 'top',
-        textStyle: {
-            color: '#fff',
-            textAlign: 'center',
-            fontSize: 24 * scale,
-            fontWeight: 'bold'
-        },
-    }],
-  
+    
     series: [
         //内圈圆环
         {
@@ -123,7 +112,7 @@ var option6 = {
             clockWise: false, //顺时加载
             hoverAnimation: false, //鼠标移入变大
             center: ['50%', '55%'],
-            radius: ['20%', '20%'],
+            radius: ['10%', '10%'],
             label: {
                 normal: {
                     show: false
@@ -144,7 +133,8 @@ var option6 = {
                 itemStyle: {
                     normal: {
                         borderWidth: 0,
-                        borderColor: 'none'
+                        borderColor: 'none',
+                        color: 'transparent'
                     }
                 }
             }]
@@ -155,26 +145,22 @@ var option6 = {
             type: 'pie',
             clockWise: false, //顺时加载
             hoverAnimation: false, //鼠标移入变大
-            startAngle: 360,
+            startAngle: 100,
             center: ['50%', '55%'],
-            radius: ['35%', '25%'],
+            radius: ['20%', '30%'],
             color: exemptcolor,
             label: {
                 normal: {
-                    formatter: function(params) {
-                        if (params.dataIndex === 0) {
-                            return '{examptdata|' + params.value + '}\n{exemptname|' + params.name + '}';
-                        }
-                    },
+                    formatter:'未列入\n维修计划',
                     rich: rich
                 }
             },
             labelLine: {
                 normal: {
-                    length: 90,
+                    length: 40,
                     length2: 20,
                     lineStyle: {
-                        color: '#ffce22',
+                        color: '#328cfe',
                     }
                 }
             },
@@ -184,27 +170,24 @@ var option6 = {
         {
             name: '正常考试',
             type: 'pie',
+            startAngle: 320,
             clockWise: false, //顺时加载
             hoverAnimation: false, //鼠标移入变大
             center: ['50%', '55%'],
-            radius: ['50%', '40%'],
+            radius: ['35%', '25%'],
             color: examNumcolor,
             label: {
                 normal: {
-                    formatter: function(params) {
-                        if (params.dataIndex === 0) {
-                            return '{examdata|' + params.value + '}\n{exemname|' + params.name + '}';
-                        }
-                    },
+                    formatter:'已列入\n维修计划',
                     rich: rich
                 }
             },
             labelLine: {
                 normal: {
-                    length: 70,
+                    length: 20,
                     length2: 20,
                     lineStyle: {
-                        color: '#328cfe',
+                        color: '#ffce22',
                     }
                 }
             },
@@ -218,7 +201,7 @@ var option6 = {
             clockWise: false, //顺时加载
             hoverAnimation: false, //鼠标移入变大
             center: ['50%', '55%'],
-            radius: ['60%', '60%'],
+            radius: ['45%', '45%'],
             label: {
                 normal: {
                     show: false
@@ -239,7 +222,8 @@ var option6 = {
                 itemStyle: {
                     normal: {
                         borderWidth: 0,
-                        borderColor: 'none'
+                        borderColor: 'none',
+                        color: 'transparent'
                     }
                 }
             }]
@@ -248,4 +232,242 @@ var option6 = {
 };
        // 使用刚指定的配置项和数据显示图表。
     myChart6.setOption(option6);
+})
+
+
+
+$(function () {
+    var myChart7 = echarts.init(document.getElementById('pieChart7'));
+/*---------------------数据----------------------------*/
+/*---------------------数据----------------------------*/
+var exemptData = [{
+    value: 60,
+    name: '已列入维修计划'
+}, {
+    value: 20,
+    name: '',
+    labelLine: {
+        normal: {
+            show: false
+        }
+    }
+}];
+var examNum = [{
+    value: 48,
+    name: '未列入维修计划',
+}, {
+    value: 58,
+    name: '',
+    labelLine: {
+        normal: {
+            show: false
+        }
+    }
+}];
+var scale = 1;
+/*---------------------颜色配置----------------------------*/
+var exemptcolor = [{
+    type: 'linear',
+    x: 0,
+    y: 0,
+    x2: 0,
+    y2: 1,
+    colorStops: [{
+        offset: 0.2,
+        color: '#328cfe' // 0% 处的颜色
+    }, {
+        offset: 1,
+        color: '#328cfe' // 100% 处的颜色
+    }],
+    globalCoord: false // 缺省为 false
+}, 'none'];
+
+var examNumcolor = [{
+    type: 'linear',
+    x: 0,
+    y: 0,
+    x2: 0,
+    y2: 1,
+    colorStops: [{
+        offset: 0.3,
+        color: '#ffce22' // 0% 处的颜色
+    }, {
+        offset: 1,
+        color: '#ffce22' // 100% 处的颜色
+    }],
+    globalCoord: false // 缺省为 false
+}, 'none'];
+/*---------------------富文本----------------------------*/
+var rich = {
+    exemptname: {
+        color: '#000',
+        fontSize: 12 * scale,
+        padding: [0, 0]
+    },
+    examptdata: {
+        color: '#000',
+        fontSize: 12 * scale,
+        padding: [0, 0],
+        fontWeight: 'bold'
+    },
+    exemname: {
+        color: '#000',
+        fontSize: 12 * scale,
+        padding: [0, 0]
+    },
+    examdata: {
+        color: '#000',
+        fontSize: 12 * scale,
+        padding: [0, 0],
+        fontWeight: 'bold'
+    },
+    rectblue: {
+        width: 14,
+        height: 14,
+        borderRadius:3,
+        backgroundColor: "#ffce22",
+    },
+    rectred: {
+        width:14,
+        height:14,
+        borderRadius:3,
+        backgroundColor:'#ffce22'
+    },
+    space: {
+        padding: [0, 10, 0, 0]
+    }
+}
+
+
+var option7 = {
+    
+    series: [
+        //内圈圆环
+        {
+            name: '内边框',
+            type: 'pie',
+            startAngle: 260,
+            clockWise: false, //顺时加载
+            hoverAnimation: false, //鼠标移入变大
+            center: ['50%', '55%'],
+            radius: ['10%', '10%'],
+            label: {
+                normal: {
+                    show: false
+                }
+            },
+            data: [{
+                value: 10,
+                name: '',
+                itemStyle: {
+                    normal: {
+                        borderWidth: 2,
+                        borderColor: '#ff6673'
+                    }
+                }
+            }, {
+                value: 2,
+                name: '',
+                itemStyle: {
+                    normal: {
+                        borderWidth: 0,
+                        borderColor: 'none',
+                        color: 'transparent'
+                    }
+                }
+            }]
+        },
+        //免考人数圆环
+        {
+            name: '免考人数',
+            type: 'pie',
+            clockWise: false, //顺时加载
+            hoverAnimation: false, //鼠标移入变大
+            startAngle: 100,
+            center: ['50%', '55%'],
+            radius: ['20%', '30%'],
+            color: exemptcolor,
+            label: {
+                normal: {
+                    formatter: '未列入\n维修计划',
+                    rich: rich
+                }
+            },
+            labelLine: {
+                normal: {
+                    length: 40,
+                    length2: 20,
+                    lineStyle: {
+                        color: '#328cfe',
+                    }
+                }
+            },
+            data: exemptData,
+        },
+        //正常考试人数圆环
+        {
+            name: '正常考试',
+            type: 'pie',
+            startAngle: 320,
+            clockWise: false, //顺时加载
+            hoverAnimation: false, //鼠标移入变大
+            center: ['50%', '55%'],
+            radius: ['35%', '25%'],
+            color: examNumcolor,
+            label: {
+                normal: {
+                    formatter: '已列入\n维修计划',
+                    rich: rich
+                }
+            },
+            labelLine: {
+                normal: {
+                    length: 20,
+                    length2: 20,
+                    lineStyle: {
+                        color: '#ffce22',
+                    }
+                }
+            },
+            data: examNum,
+        },
+        //内圈圆环
+        {
+            name: '外边框',
+            type: 'pie',
+            // startAngle: 260,
+            clockWise: false, //顺时加载
+            hoverAnimation: false, //鼠标移入变大
+            center: ['50%', '55%'],
+            radius: ['45%', '45%'],
+            label: {
+                normal: {
+                    show: false
+                }
+            },
+            data: [{
+                value: 9,
+                name: '',
+                itemStyle: {
+                    normal: {
+                        borderWidth: 2,
+                        borderColor: '#328cfe'
+                    }
+                }
+            }, {
+                value: 2,
+                name: '',
+                itemStyle: {
+                    normal: {
+                        borderWidth: 0,
+                        borderColor: 'none',
+                        color: 'transparent'
+                    }
+                }
+            }]
+        },
+    ]
+};
+       // 使用刚指定的配置项和数据显示图表。
+    myChart7.setOption(option7);
 })
